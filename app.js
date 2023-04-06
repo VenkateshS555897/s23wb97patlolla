c9ded289b7cde6472268549bb90db15fd00aecb5
VenkateshS555897 <venkateshreddy0918@gmail.com>
2023-04-05 23:29:05 -0500

    final

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var selectorRouter = require('./routes/grass');
+var boardRouter = require('./routes/board');
+var Router = require('./routes/Selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/grass', selectorRouter);
+app.use('/board', boardRouter);
+app.use('/Selector', Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
